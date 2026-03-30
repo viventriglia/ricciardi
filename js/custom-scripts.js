d571f4baacf6c020a245e84c585a219cdebb8c2f
Vincenzo <vincenzo.ventriglia@outlook.com>
2026-03-30 20:51:11 +0200

    Remove deprecated JavaScript files and add robots.txt and sitemap.xml for SEO optimization
    - Deleted jquery.prettyphoto.js, mousescroll.js, and smoothscroll.js as they are no longer needed.
    - Added robots.txt to allow search engines to index the site and provide a link to the sitemap.
    - Created sitemap.xml to help search engines understand the site structure and improve crawling efficiency.

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -187,49 +187,27 @@ jQuery(function($) {
         };
     })();
 
-    var navGlow = (function() {
-        if (!window.matchMedia('(hover: hover) and (pointer: fine)').matches) {
-            return null;
-        }
-
-        var links = document.querySelectorAll('#main-nav .navbar-nav > li > a');
-        if (!links.length) {
-            return null;
-        }
-
-        Array.prototype.forEach.call(links, function(link) {
-            link.addEventListener('mousemove', function(event) {
-                var bounds = link.getBoundingClientRect();
-                var x = ((event.clientX - bounds.left) / bounds.width) * 100;
-                var y = ((event.clientY - bounds.top) / bounds.height) * 100;
-
-                link.style.setProperty('--nav-glow-x', x + '%');
-                link.style.setProperty('--nav-glow-y', y + '%');
-            });
-
-            link.addEventListener('mouseleave', function() {
-                link.style.removeProperty('--nav-glow-x');
-                link.style.removeProperty('--nav-glow-y');
-            });
-        });
-
-        return true;
-    })();
-
     $(window).scroll(function(event) {
         Scroll();
         updateBackToTop();
     });
 
-    $('.navbar-collapse ul li a').on('click', function() {
+    $('a[href^="#"]').not('[href="#"]').on('click', function(event) {
+        var $target = $(this.hash);
+
+        if (!$target.length) {
+            return;
+        }
+
+        event.preventDefault();
+
         var link = this;
         $('html, body').animate({
-            scrollTop: $(this.hash).offset().top - 5
+            scrollTop: $target.offset().top - 5
         }, 1000);
         window.setTimeout(function() {
             link.blur();
         }, 120);
-        return false;
     });
  
     function Scroll() {
@@ -275,86 +253,10 @@ jQuery(function($) {
         $('#tohash').toggleClass('is-visible', $(window).scrollTop() > 500);
     }
 
-    $('#tohash').on('click', function() {
-        $('html, body').animate({
-            scrollTop: $(this.hash).offset().top - 5
-        }, 1000);
-        return false;
-    });
-
     updateBackToTop();
 
   
     new WOW().init();
-     
-    smoothScroll.init();
-
-    
-    $(window).load(function() {
-        'use strict';
-        var $portfolio_selectors = $('.portfolio-filter >li>a');
-        var $portfolio = $('.portfolio-items');
-        $portfolio.isotope({
-            itemSelector: '.portfolio-item',
-            layoutMode: 'fitRows'
-        });
-
-        $portfolio_selectors.on('click', function() {
-            $portfolio_selectors.removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $portfolio.isotope({
-                filter: selector
-            });
-            return false;
-        });
-    });
-
-    $(document).ready(function() {
-   
-        $.fn.animateNumbers = function(stop, commas, duration, ease) {
-            return this.each(function() {
-                var $this = $(this);
-                var start = parseInt($this.text().replace(/,/g, ""));
-                commas = (commas === undefined) ? true : commas;
-                $({
-                    value: start
-                }).animate({
-                    value: stop
-                }, {
-                    duration: duration == undefined ? 1000 : duration,
-                    easing: ease == undefined ? "swing" : ease,
-                    step: function() {
-                        $this.text(Math.floor(this.value));
-                        if (commas) {
-                            $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-                        }
-                    },
-                    complete: function() {
-                        if (parseInt($this.text()) !== stop) {
-                            $this.text(stop);
-                            if (commas) {
-                                $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-                            }
-                        }
-                    }
-                });
-            });
-        };
-
-        $('.business-stats').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-            var $this = $(this);
-            if (visible) {
-                $this.animateNumbers($this.data('digit'), false, $this.data('duration'));
-                $this.unbind('inview');
-            }
-        });
-    });
-
- 
-    $("a[rel^='prettyPhoto']").prettyPhoto({
-        social_tools: false
-    });
  
 
 });
